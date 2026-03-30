9c00fea7bdb8bf7e7aac5dbf67e30e67d8a3e50a
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-03-30 18:56:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -4,7 +4,7 @@ class Solution {
         char arr1[]=new char[26];
         char arr2[]=new char[26];
         for(int i=0;i<n;i++){
-            char ch=s2.charAt(i);
+            char ch=s1.charAt(i);
             if(i%2==0){
                 arr2[ch-'a']++;
             }
@@ -13,7 +13,7 @@ class Solution {
             }
         }
         for(int i=0;i<n;i++){
-            char ch=s1.charAt(i);
+            char ch=s2.charAt(i);
             if(i%2==0){
                 if(arr2[ch-'a']<=0){
                     return false;
