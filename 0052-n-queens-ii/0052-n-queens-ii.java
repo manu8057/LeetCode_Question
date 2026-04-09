9613784b7486760daa98e442b7e752235aac4f31
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-04-09 12:26:24 +0530

    Time: 2 ms (59.85%), Space: 42.1 MB (63.6%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public boolean check(int r,int c,int n,int arr[][]){
+        for(int i=0;i<n;i++){
+            if(arr[i][c]==1){
+                return false;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[r][i]==1){
+                return false;
+            }
+        }
+        int i=r;
+        int j=c;
+        while(i>=0 && j>=0){
+            if(arr[i][j]==1){
+                return false;
+            }
+            i--;
+            j--;
+        }
+        i=r;
+        j=c;
+        while(i<n && j<n){
+            if(arr[i][j]==1){
+                return false;
+            }
+            i++;
+            j++;
+        }
+        i=r;
+        j=c;
+        while(i>=0 && j<n){
+            if(arr[i][j]==1){
+                return false;
+            }
+            i--;
+            j++;
+        }
+        i=r;
+        j=c;
+        while(i<n && j>=0){
+            if(arr[i][j]==1){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public int fun(int id,int n,int arr[][]){
+        if(id==n){
+            return 1;
+        }
+        int a=0;
+        for(int i=0;i<n;i++){
+            if(check(id,i,n,arr)){
+                arr[id][i]=1;
+                a+=fun(id+1,n,arr);
+                arr[id][i]=0;
+            }
+        }
+        return a;
+    }
+    public int totalNQueens(int n) {
+        int arr[][]=new int[n][n];
+        return fun(0,n,arr);
+    }
+}
