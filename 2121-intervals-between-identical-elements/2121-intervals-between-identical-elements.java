258b0e01e95e3e70177fc7116be2ca5fcdbf144f
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-04-23 21:31:48 +0530

    Time: 163 ms (5.15%), Space: 191.1 MB (5.15%) - LeetHub

---
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long[] getDistances(int[] arr) {
+        HashMap<Integer,Long>map2=new HashMap<>();
+        HashMap<Integer,Integer>map1=new HashMap<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            map1.put(arr[i],map1.getOrDefault(arr[i],0)+1);
+            map2.put(arr[i],map2.getOrDefault(arr[i],0L)+i);
+        }
+        HashMap<Integer,Long>ls=new HashMap<>();
+        HashMap<Integer,Integer>lc=new HashMap<>();
+        long ans[]=new long[n];
+        for(int i=0;i<n;i++){
+            int v=arr[i];
+            long s=ls.getOrDefault(v,0L);
+            int c=lc.getOrDefault(v,0);
+            long rs=map2.get(v)-s-i;
+            int rc=map1.get(v)-c-1;
+            long l=i*(long)c-s;
+            long rr=rs-i*(long)rc;
+            ans[i]=l+rr;
+            ls.put(v,s+i);
+            lc.put(v,c+1);
+        }
+        return ans;
+    }
+}
