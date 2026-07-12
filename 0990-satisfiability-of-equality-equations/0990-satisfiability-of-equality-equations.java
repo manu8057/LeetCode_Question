93ca1c0060f94cb19692eacceef0454039b47de4
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-07-12 12:25:53 +0530

    Time: 1 ms (97.45%), Space: 44 MB (30.01%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int find(int i,int pr[]){
+        if(pr[i]==i) return i;
+        return pr[i]=find(pr[i],pr);
+    }
+    public boolean equationsPossible(String[] equations) {
+        int pr[]=new int[26];
+        int fr[]=new  int[26];
+        for(int i=0;i<26;i++){
+            pr[i]=i;
+            fr[i]=1;
+        }
+        for(String s:equations){
+            int a=s.charAt(0)-'a';
+            int b=s.charAt(3)-'a';
+            char c=s.charAt(1);
+            int ab=find(a,pr);
+            int bb=find(b,pr);
+            if(c=='=' && ab!=bb){
+                if(fr[ab]>fr[bb]){
+                    pr[bb]=pr[ab];
+                    fr[ab]+=fr[bb];
+                }
+                else{
+                    pr[ab]=pr[bb];
+                    fr[bb]+=fr[ab];
+                }
+            }
+        }
+        for(String str:equations){
+            int a=str.charAt(0)-'a';
+            int b=str.charAt(3)-'a';
+            int c=str.charAt(1);
+            if(c=='!'){
+                if(pr[a]==pr[b]) return false;
+            }
+        }
+        return true;
+    }
+}
