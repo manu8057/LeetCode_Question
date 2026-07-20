b47e4a205d3074135a6deba8f6bf0df3dfa6a277
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-07-20 10:17:10 +0530

    Time: 33 ms (30.79%), Space: 53.1 MB (10.32%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,22 +1,25 @@
 class Solution {
-    // Boolean dp[][][][];
+    HashMap<String,Boolean>map=new HashMap<>();
     public boolean fun(int i,int n,int nums[],int d,int r,int t){
         if(i>r){
             return d>=0;
         }
-        // if(dp[i][d][r][t]!=null){
-        //     return dp[][][][];
-        // }
+        if(map.containsKey(i+"^"+d+"/"+r+"{"+t)){
+            return map.get(i+"^"+d+"/"+r+"{"+t);
+        }
         if(t==0){
-            return fun(i+1,n,nums,d+nums[i],r,1) || fun(i,n,nums,d+nums[r],r-1,1);
+            boolean ans=fun(i+1,n,nums,d+nums[i],r,1) || fun(i,n,nums,d+nums[r],r-1,1);
+            map.put(i+"^"+d+"/"+r+"{"+t,ans);
+            return ans;
         }
         else{
-            return fun(i+1,n,nums,d-nums[i],r,0) && fun(i,n,nums,d-nums[r],r-1,0);
+            boolean ans=fun(i+1,n,nums,d-nums[i],r,0) && fun(i,n,nums,d-nums[r],r-1,0);
+            map.put(i+"^"+d+"/"+r+"{"+t,ans);
+            return ans;
         }
     }
     public boolean predictTheWinner(int[] nums){
         int n=nums.length;
-        // dp=new Boolean[n+1][][][];
         return fun(0,nums.length,nums,0,nums.length-1,0);
     }
 }
