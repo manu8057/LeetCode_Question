685abe2a0f70c66e70f73e1ab83908c12196ffb7
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-07-20 22:21:05 +0530

    Time: 32 ms (35.62%), Space: 90.5 MB (82.52%) - LeetHub

---
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n1=g.length;
+        int n2=s.length;
+        int i=n2-1;
+        int j=n1-1;
+        int c=0;
+        while(j>=0 && i>=0){
+            if(g[j]<=s[i]){
+                j--;
+                i--;
+                c++;
+            }
+            else{
+                j--;
+            }
+        }
+        return c;
+    }
+}
