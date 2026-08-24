52e8333026510bdc12543ac0970beccde75a34d3
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-08-25 00:50:16 +0530

    Time: 7 ms (56.38%), Space: 46.9 MB (78.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,31 @@
 class Solution {
-    public boolean canFinish(int num, int[][] arr) {
-        int n=arr.length;
-        if(n==0){
-            return true;
-        }
+    public boolean canFinish(int n, int[][] arr) {
+        int fr[]=new int[n];
         ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<num;i++){
+        for(int  i=0;i<n;i+=1){
             adj.add(new ArrayList<>());
         }
-        int f[]=new int[num];
         for(int ar[]:arr){
             adj.get(ar[1]).add(ar[0]);
-            f[ar[0]]++;
+            fr[ar[0]]++;
         }
         Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<num;i++){
-            if(f[i]==0){
+        for(int i=0;i<n;i+=1){
+            if(fr[i]==0){
                 q.add(i);
             }
         }
-        if(q.size()==0){
-            return false;
-        }
-        ArrayList<Integer>l=new ArrayList<>();
+        int c=0;
         while(!q.isEmpty()){
-            int el=q.poll();
-            l.add(el);
-            for(int x:adj.get(el)){
-                f[x]--;
-                if(f[x]==0){
-                    q.add(x);
+            int u=q.poll();
+            c+=1;
+            for(int el:adj.get(u)){
+                fr[el]--;
+                if(fr[el]==0){
+                    q.add(el);
                 }
             }
         }
-        return l.size()==num;
+        return c==n;
     }
 }
