f8c0aa50980069d76f9f2863691ec557c7537595
Manoj Sharma <144920847+manu8057@users.noreply.github.com>
2026-08-30 13:57:13 +0530

    Time: 2 ms (100%), Space: 86.7 MB (40%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,26 +1,23 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
         int n=nums.length;
-        int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            map.put(nums[i],i);
-            max=Math.max(max,nums[i]);
-            min=Math.min(min,nums[i]);
+        int max=Integer.MIN_VALUE;
+        int id1=0;
+        int id2=0;
+        for(int i=0;i<n;i+=1){
+            if(nums[i]>max){
+                id2=i;
+                max=nums[i];
+            }
+            if(nums[i]<min){
+                id1=i;
+                min=nums[i];
+            }
         }
-        int a=Math.max(map.get(min),map.get(max))+1;
-        int b=n-Math.min(map.get(min),map.get(max));
-        int c=0;
-        if(map.get(min)<map.get(max)){
-            c+=map.get(min)+1;
-            c+=n-map.get(max);
-        }
-        else{
-            c+=map.get(max)+1;
-            c+=n-map.get(min);
-        }
-        if(map.get(min)==map.get(max)) c-=1;
-        return Math.min(a,Math.min(b,c));
+        int t1=Math.max(id1,id2)+1;
+        int t2=n-Math.min(id1,id2);
+        int t3=Math.min(id1,id2)+1+n-Math.max(id1,id2);
+        return Math.min(t1,Math.min(t2,t3));
     }
 }
